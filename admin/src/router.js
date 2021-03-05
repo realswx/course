a21898c5cf352319aeb728e2d498321f3035d0bf
realswx <sangwx23@hotmail.com>
2021-03-05 16:56:03 +0800

    1(2).增加admin页面 2(2).增加路由配置，访问/admin时，加载admin组件

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login'
+import Admin from './views/Admin'
 
 Vue.use(Router);
 
@@ -13,5 +14,8 @@ export default new Router({
   }, {
     path: '/login',
     component: Login
+  }, {
+      path: '/admin',
+      component: Admin
   }]
 })
