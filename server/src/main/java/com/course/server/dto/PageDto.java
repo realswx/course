bc91d4b830e35c8c3071e492456027b43e74104d
realswx <sangwx23@hotmail.com>
2021-03-09 17:09:25 +0800

    1.分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

---
+++ server/src/main/java/com/course/server/dto/PageDto.java
@@ -0,0 +1,48 @@
+package com.course.server.dto;
+
+import java.util.List;
+
+public class PageDto<T> {
+    // 当前页码
+    protected int pageNum;
+
+    // 每页条数
+    protected int pageSize;
+
+    // 总条数
+    protected long total;
+
+    protected List<T> list;
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +21,15 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
-        PageHelper.startPage(1, 1);
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPageNum(), pageDto.getPageSize());
         ChapterExample chapterExample = new ChapterExample();
 //        chapterExample.createCriteria().andIdEqualTo("1"); //createCriteria()相当于where。查询id为1的记录
 //        chapterExample.setOrderByClause("id desc");
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtoList = new ArrayList<>();
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
 //        for(int i = 0, l = chapterList.size(); i < l; i++){
 //            Chapter chapter = chapterList.get(i);
 //            ChapterDto chapterDto = new ChapterDto();
@@ -38,6 +42,7 @@ public class ChapterService {
                     BeanUtils.copyProperties(chapter, chapterDto);
                     return chapterDto;
                 }).collect(Collectors.toList());
-        return chapterDtoList;
+        pageDto.setList(chapterDtoList);
+//        return pageDto;
     }
 }
