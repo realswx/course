06817b0d74e32360f483376755ea0fd36adb0f46
realswx <sangwx23@hotmail.com>
2021-03-11 00:06:11 +0800

    1.增加统一返回实体ResponseDto，前后端代码针对ResponseDto做修改 2.chapter保存成功后关闭表单，并刷新列表 3.为modal增加id属性

--- admin/src/views/admin/Chapter.vue
+++ admin/src/views/admin/Chapter.vue
@@ -96,7 +96,7 @@
 
       </tbody>
     </table>
-    <div class="modal fade" tabindex="-1" role="dialog">
+    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -151,7 +151,7 @@
     methods: {
       add() {
         let _this = this;
-        $(".modal").modal("show");
+        $("#form-modal").modal("show");
       },
 
       list(pageNum) {
@@ -161,8 +161,9 @@
           pageSize: _this.$refs.pagination.pageSize,
         }).then((response) => {
           console.log("查询大章列表结果：", response);
-          _this.chapters = response.data.list;
-          _this.$refs.pagination.render(pageNum, response.data.total);
+          let responseData = response.data;
+          _this.chapters = responseData.content.list;
+          _this.$refs.pagination.render(pageNum, responseData.content.total);
         })
       },
 
@@ -171,6 +172,11 @@
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
                 _this.chapter).then((response) => {
           console.log("保存大章列表结果：", response);
+          let responseData = response.data;
+          if (responseData.success) {
+            $("#form-modal").modal("hide");
+            _this.list(_this.$refs.pagination.pageNum);
+          }
         })
       }
     }
