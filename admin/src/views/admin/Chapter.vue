970016c13c44fe6911694d309c1efcb0e07b71dd
realswx <sangwx23@hotmail.com>
2021-03-12 11:25:30 +0800

    1.增加工具类tool.js和校验类validator.js 2.大章保存非空和长度增加校验

--- admin/src/views/admin/Chapter.vue
+++ admin/src/views/admin/Chapter.vue
@@ -141,7 +141,7 @@
     methods: {
       add() {
         let _this = this;
-        // _this.$data.chapter = _this.$options.data().chapter;
+        _this.$data.chapter = _this.$options.data().chapter;
         // _this.chapter = {};
         $("#form-modal").modal("show");
       },
@@ -161,16 +161,22 @@
         })
       },
 
-      save() {
-        Loading.show();
+      save(pageNum) {
         let _this = this;
+        //校验
+        if (!Validator.require(_this.chapter.name, "名称")
+                || !Validator.require(_this.chapter.courseId, "课程ID")
+                || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
+          return;
+        }
+        Loading.show();
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
                 _this.chapter).then((response) => {
                   Loading.hide();
           console.log("保存大章列表结果：", response);
           let responseData = response.data;
           if (responseData.success) {
-            _this.$data.chapter = _this.$options.data().chapter;
+            // _this.$data.chapter = _this.$options.data().chapter;
             $("#form-modal").modal("hide");
             _this.list(_this.$refs.pagination.pageNum);
             Toast.success("保存成功！");
